33925287cedd6507a2e4f8686d7a54d4e6c5fed5
mn1del <davidleech@gmail.com>
2016-10-24 16:22:50 +0100

    debug syntax

--- cnc.js
+++ cnc.js
@@ -175,7 +175,7 @@ function init() {
     geom3 = THREE.CSG.fromCSG(xBScrwCsg);
     var xBScrw = new THREE.Mesh(geom3,matAluminium);
     xBScrwFixSupp.add(xBScrw);  
-    xBScrw.position.set(0,0,xBScrewFixSuppObj.bscrewZPos);
+    xBScrw.position.set(0,0,xBScrwFixSuppObj.bscrewZPos);
     
     // Make base
 //     var geom = baseObj.makeCsg().center("y");
