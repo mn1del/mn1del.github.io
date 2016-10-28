191f259d9c8e4a454860d788f53ac0475bfe2ec0
mn1del <davidleech@gmail.com>
2016-10-28 15:18:34 +0100

    tweak lights

--- cnc.js
+++ cnc.js
@@ -135,15 +135,20 @@ function init() {
 
     //create light(s)
     var dirLight = new THREE.DirectionalLight(colWhite , 0.95);
-    dirLight.position.set(500, -300, 1000);
+    dirLight.position.set(500, -300, 500);
     dirLight.position.normalize();
     scene.add(dirLight);
 
     var dirLight1 = new THREE.DirectionalLight(colWhite , 0.95);
-    dirLight1.position.set(-200, -300, -1000);
+    dirLight1.position.set(-200, -300, -500);
     dirLight1.position.normalize();
     scene.add(dirLight1);
 
+    var dirLight2 = new THREE.DirectionalLight(colWhite , 0.95);
+    dirLight2.position.set(-500, -300, 500);
+    dirLight2.position.normalize();
+    scene.add(dirLight2);
+    
 /*     var pointLight = new THREE.PointLight(colWhite , 5, 50);
     pointLight.position.set(10, 20, -1000);
     scene.add(pointLight); */
